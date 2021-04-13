bf05214f20da7b3002320f942170b24152cffec9
Hanefors Victor <victor.hanefors@scania.com>
2021-04-13 15:29:46 +0200

    Debugging

--- rpi/model_functions.py
+++ rpi/model_functions.py
@@ -23,14 +23,15 @@ def depth_volume(depth):
 
 def depth_to_xyz(depth):
     x_size = depth.shape[1]
+    y_size = depth.shape[0]
+
     x = np.asarray([i - (x_size // 2) for i in range(x_size)])
-    x = np.tile(x, (1, x_size))
-    x = np.transpose(x)
+    x = np.tile(np.expand_dims(x, axis=0), (1, x_size))
     x = np.tan(cfg["webcam_h_fov"] * pi / 360) / (x_size / 2) * np.multiply(x, depth)
 
-    y_size = depth.shape[0]
     y = np.asarray([i - (y_size // 2) for i in range(y_size)])
-    y = np.tile(y, (1, y_size))
+    y = np.tile(np.expand_dims(y, axis=0), (1, y_size))
+    y = np.transpose(y)
     y = np.tan(cfg["webcam_h_fov"] * pi / 360) / (y_size / 2) * np.multiply(y, depth)
 
     z = depth  # TODO Might translate the point cloud along the z-axis, so that camera is not z=0
